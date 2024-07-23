349d8f3067766000718d9414b7e70b6269de91bb
Ayush <ayushthapa0010@gmail.com>
2024-07-23 15:22:46 +0545

    verify email

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -1,3 +1,54 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 
+export async function POST(request:Request) {
+    await dbConnect()
+    
+    try {
+        const {username,code}=await request.json()
+
+        const decodedUsername=decodeURIComponent(username)
+        const user=await UserModel.findOne({username:decodedUsername})
+
+        if(!user){
+            return Response.json({
+                success:false,
+                message:"User not found",
+            },
+            {status:400})
+        }
+
+        const isCodeValid=user.verifyCode===code
+        const isCodeNotExpiried=new Date(user.verifyCodeExpiry)>new Date();
+
+        if(isCodeValid && isCodeNotExpiried){
+            user.isVerified=true
+            await user.save()
+            return Response.json({success:true,
+                message:"Account verified successfully"
+            },{status:200})
+        }
+        else if(!isCodeNotExpiried){
+            return Response.json({
+                success:false,
+                message:"Verification code has expired, please signup again",
+            },
+        {status:400})
+        }
+        else{
+            return Response.json({
+                success:false,
+                message:"Incorrect Verification code",
+            },
+        {status:400})
+        }
+
+    } catch (error) {
+        console.error("Error verifying user",error);
+        return Response.json({
+            success:false,
+            message:"Error verifying user",
+        },
+    {status:500})
+    }
+}
